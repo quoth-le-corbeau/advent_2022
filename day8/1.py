82fda9bca19970fa7533bd1f8a2078b3ade80153
quoth-the-macbook <amgorman@tutanota.com>
2024-01-13 21:05:44 +0100

    day8p1

--- day8/1.py
+++ day8/1.py
@@ -1,17 +1,24 @@
 import os
+import re
 
 import helpers
 
 
-def RENAME_FUNC(file_path: os.path):
-    RENAME = _RENAME_FUNC(file=file_path)
-    pass
+def count_visible_trees(file_path: os.path):
+    grid = _get_tree_height_grid(file=file_path)
+    return _count_visible_trees_in_grid(grid=grid)
 
 
-def _RENAME_FUNC(file: os.path):
+def _count_visible_trees_in_grid(grid: list[list[int]]) -> int:
+    return 1
+
+def _get_tree_height_grid(file: os.path) -> list[list[int]]:
     with open(file) as puzzle_input:
-        lines = puzzle_input.read()
-        print(lines)
+        lines = puzzle_input.read().splitlines()
+        grid = list()
+        for line in lines:
+            grid.append(list(map(int, re.findall(r"\d+", line))))
+        return grid
 
 
-helpers.print_timed_results(solution_func=RENAME_FUNC)
+helpers.print_timed_results(solution_func=count_visible_trees)
