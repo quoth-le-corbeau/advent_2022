0fa8046c26908f6b42f01efaf9c14e531f4d4f83
quoth-the-macbook <amgorman@tutanota.com>
2024-01-13 17:13:56 +0100

    day7 build nested dict structure

--- day7/1.py
+++ day7/1.py
@@ -2,59 +2,42 @@ import os
 
 import helpers
 
-GOAL = {
-    "/": {
-        "a": {
-            "e": {"i": 584},
-            "f": 29116,
-            "g": 2557,
-            "h.lst": 62596,
-        },
-        "b.txt": 14848514,
-        "c.dat": 8504156,
-        "d": {
-            "j": 4060174,
-            "d.log": 8033020,
-            "d.ext": 5626152,
-            "k": 7214296,
-        },
-    },
-}
-
-TARGET_DIRS = list()
-TARGET_SIZE = 100000
+TARGET_DIRECTORY_SIZE = 100000
+TARGET_DIRECTORIES = list()
 
 
-def sum_largest_directories(file_path: os.path):
-    file_structure = _get_structure(file=file_path)
-    print(file_structure)
-    for key, value in file_structure["/"].items():
+def sum_targed_directories(file_path: os.path):
+    file_structure = _build_nested_file_structure(file=file_path)
+    for key, value in file_structure.items():
         if isinstance(value, int):
             continue
-        elif isinstance(value, dict):
-            dir_size = _recursive_sum(directory=value)
-            if dir_size <= TARGET_SIZE:
-                TARGET_DIRS.append(dir_size)
-    return sum(TARGET_DIRS)
-
-
-def _recursive_sum(directory):
-    size = 0
-    for key, value in directory.items():
-        if not isinstance(value, dict):
-            size += value
         else:
-            next_directory_sum = _recursive_sum(directory=value)
-            size += next_directory_sum
-            if next_directory_sum <= TARGET_SIZE:
-                TARGET_DIRS.append(next_directory_sum)
-    return size
+            # assume values can only be integers or dictionaries
+            size_of_next_inner_directory = _recursive_sum_of_nested_dicts(nested_dict=value)
+            if size_of_next_inner_directory <= TARGET_DIRECTORY_SIZE:
+                TARGET_DIRECTORIES.append(size_of_next_inner_directory)
+    return sum(TARGET_DIRECTORIES)
 
 
-def _get_structure(file: os.path):
+def _recursive_sum_of_nested_dicts(nested_dict) -> int:
+    sum_of_integers_in_outer_dict = 0
+    for key, value in nested_dict.items():
+        if isinstance(value, int):
+            sum_of_integers_in_outer_dict += value
+        else:
+            sum_of_integers_in_next_inner_dict = _recursive_sum_of_nested_dicts(
+                nested_dict=value
+            )
+            if sum_of_integers_in_next_inner_dict <= TARGET_DIRECTORY_SIZE:
+                TARGET_DIRECTORIES.append(sum_of_integers_in_next_inner_dict)
+            sum_of_integers_in_outer_dict += sum_of_integers_in_next_inner_dict
+    return sum_of_integers_in_outer_dict
+
+
+def _build_nested_file_structure(file: os.path):
     with open(file) as puzzle_input:
         lines = puzzle_input.read().splitlines()
-        current_directory = dict()
+        current_dir = dict()
         stack = list()
         for line in lines:
             if line.split()[-1] == "ls":
@@ -62,28 +45,24 @@ def _get_structure(file: os.path):
             elif line.split()[1] == "cd":
                 directory_name = line.split()[-1]
                 if directory_name == "/":
-                    current_directory = dict()
+                    current_dir = dict()
                     stack = list()
                 elif directory_name == "..":
-                    current_directory = stack.pop()
+                    current_dir = stack.pop()
                 else:
-                    if directory_name not in current_directory:
-                        current_directory[directory_name] = dict()
-                    stack.append(current_directory)
-                    current_directory = current_directory[directory_name]
+                    if directory_name not in current_dir:
+                        current_dir[directory_name] = dict()
+                    stack.append(current_dir)
+                    current_dir = current_dir[directory_name]
             else:
-                size_or_dir, name = line.split()
-                if size_or_dir == "dir":
-                    current_directory[name] = dict()
+                dir_or_file_size, dir_or_file_name = line.split()
+                if dir_or_file_size == "dir":
+                    current_dir[dir_or_file_name] = dict()
                 else:
-                    current_directory[name] = int(size_or_dir)
-
-        return {"/": stack[0]}
-
+                    current_dir[dir_or_file_name] = int(dir_or_file_size)
+        return stack[0]
 
 
-
-helpers.print_timed_results(solution_func=sum_largest_directories)
-# helpers.print_timed_results(
-#     solution_func=sum_largest_directories, test_path_extension=None
-# )
+helpers.print_timed_results(
+    solution_func=sum_targed_directories, test_path_extension=None
+)
