ed4f1bbf3cbdf649427a72ac65a6531840368173
quoth-the-macbook <amgorman@tutanota.com>
2024-01-17 23:46:00 +0100

    Continue day9p1

--- day9/1.py
+++ day9/1.py
@@ -1,9 +1,9 @@
+import math
 import os
 from dataclasses import dataclass
 import helpers
 
 
-@dataclass(frozen=True)
 class Head:
     x: int
     y: int
@@ -18,13 +18,31 @@ class Head:
         elif direction == "L":
             self.x -= spaces
 
-@dataclass(frozen=True)
+
 class Tail(Head):
 
     def follow_head(self, head: Head):
+        horizontal_distance = abs(self.x - head.x)
+        vertical_distance = abs(self.y - head.y)
+        hypoteneuse = math.sqrt(horizontal_distance ** 2 + vertical_distance ** 2)
+        move_condition = hypoteneuse > math.sqrt(2)
+        is_horizontal_move = self.y - head.y == 0
+        is_vertical_move = self.x - head.x == 0
+        is_diagonal_move = not (is_horizontal_move or is_vertical_move)
         head_coordinates = head.x, head.y
-        is_horizontal = self.y - head.y == 0
-        is_vertical = self.x - head.x == 0
+        if move_condition and is_diagonal_move:
+            self.x, self.y = head_coordinates
+        elif horizontal_distance > 1:
+            assert vertical_distance == 0
+            if self.x > head.x:
+                self.x -= 1
+            else:
+                self.x += 1
+        else:
+            if self.y > head.y:
+                self.y -= 1
+            else:
+                self.y += 1
 
 
 
